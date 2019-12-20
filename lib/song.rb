9009d2e9847de3cf3f67ff3103210e9bfb050bcd
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-20 04:06:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,4 +41,16 @@ end
   hash
 end 
 
+def self.artist_count
+  artist_hash = {}
+  @@artists.each do |artist|
+    if artist_hash[artist]
+      artist_hash[artist] += 1 
+    else 
+      artist_hash[artist] = 1 
+    end 
+  end 
+  artist_hash
+end 
+
 end 
